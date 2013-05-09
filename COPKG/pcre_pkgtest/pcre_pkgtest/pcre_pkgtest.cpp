039583cb71b8335a6edb4fa4335531242d3bb1f2
Ben Herila <ben@herila.net>
2013-05-09 12:26:28 -0700

    Updated test suite

--- COPKG/pcre_pkgtest/pcre_pkgtest/pcre_pkgtest.cpp
+++ COPKG/pcre_pkgtest/pcre_pkgtest/pcre_pkgtest.cpp
@@ -2,18 +2,19 @@
 //
 
 #include "stdafx.h"
-#include "Win32\v110\static\Release\pcre.h"
 #include <string.h>
+#include <assert.h>
+#include "pcre.h"
 
-void test(char *input)
+void test(char *input, char *result)
 {
 	pcre *myregexp;
 	const char *error;
 	int erroroffset;
 	int offsetcount;
 	int offsets[30]; // (max_capturing_groups+1)*3
-	char *result;
 	myregexp = pcre_compile("^(.*?)hello.*?(\\d+)", 0, &error, &erroroffset, NULL);
+	assert(myregexp != NULL);
 	if (myregexp != NULL) {
 		offsetcount = pcre_exec(myregexp, NULL, input, strlen(input), 0, 0, offsets, 30);
 		if (offsetcount > 0) {
@@ -28,11 +29,21 @@ void test(char *input)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	char* test1 = "This is a test of the hello world application. 123456789.";
-	char* test2 = "This string doesn't match the desired regex.";
+	char *test1 = "This is a test of the hello world application. 123456789.";
+	char *test2 = "This string doesn't match the desired regex.";
 
-	test(test1);
-	test(test2);
+	char *result = (char*)malloc(sizeof(char)*100);
+	result[0] = 0;
+
+	test(test1, result);
+	printf(result);
+	printf("\n");
+	
+	test(test2, result);
+	printf(result);
+	printf("\n");
+
+	getchar();
 
 	return 0;
 }
